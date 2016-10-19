49c3092a73f8dde568889da34eb04b76797d863c
Schmid <Katharina.Schmid@haw-hamburg.de>
2016-10-19 15:14:26 +0200

    added open menu

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -6,9 +6,30 @@ import java.io.*;
 import javax.imageio.*; 
  
 public class ImageEditorFrame extends JFrame{    
-	public ImageEditorFrame(){       
+	public ImageEditorFrame(){
+	createMenuBar();
 	setDefaultCloseOperation(EXIT_ON_CLOSE);       
 	setSize(400,  300);       
 	setVisible(true);    
 	}
+	
+	private void createMenuBar(){         
+		JMenuBar menuBar = new JMenuBar();   
+		setJMenuBar(menuBar);   	
+		JMenu menuFile = new JMenu("File");   
+		menuBar.add(menuFile);   
+		JMenuItem menuItemOpen = new JMenuItem("Open");   
+		menuFile.add(menuItemOpen);   
+		menuItemOpen.addActionListener(     new ActionListener() { 
+
+			public void actionPerformed(ActionEvent e){            
+				onOpen();          
+			}       
+		}); 
+	} 
+	
+	private void onOpen(){    
+		JOptionPane.showMessageDialog(this, "Open Selected"); 
+	}
+	
 } 
